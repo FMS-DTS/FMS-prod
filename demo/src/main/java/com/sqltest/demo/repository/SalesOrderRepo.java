27c28ca1a422f1ed0ca1ae0d4c874a71b318f1d0
Amit Hegde <amit.is21@bmsce.ac.in>
2024-06-11 15:07:12 +0530

    Update

--- demo/src/main/java/com/sqltest/demo/repository/SalesOrderRepo.java
+++
@@ -1,7 +0,0 @@
-package com.sqltest.demo.repository;
-
-import com.sqltest.demo.model.SalesOrder;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SalesOrderRepo extends JpaRepository<SalesOrder,Long> {
-}
